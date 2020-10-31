d0670ab72b5cfbc3657a6c189f1c1126c9c66608
SlowDeepCoder <danel.lindgren@gmail.com>
2020-10-31 14:02:25 +0100

    Updated the toolbar and added the option to sign ou

--- app/src/main/java/com/example/splittab/MainActivity.java
+++ app/src/main/java/com/example/splittab/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.splittab;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.splittab.ui.main.SectionsPagerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +33,39 @@ public class MainActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
 
 
+        setToolbar();
+
+
+
     }
+
+    private void setToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.actionbar);
+        toolbar.setTitle(R.string.app_title);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.actionbarGroup:
+                        return true;
+
+                    case R.id.actionbarSettings:
+                        Toast.makeText(MainActivity.this, "heej", Toast.LENGTH_LONG).show();
+                        return true;
+                    case R.id.actionbarSignOut:
+                        FirebaseAuth.getInstance().signOut();
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                        return true;
+
+                }
+                return false;
+            }
+        });
+    }
+
+
+
 }
