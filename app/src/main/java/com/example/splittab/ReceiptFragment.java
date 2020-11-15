fb63e8f19ae0a4956bf6e3efe8a5102027b65c21
Sebastian Warvsten <sebastian.warvsten@iths.se>
2020-11-15 19:02:12 +0100

    La till kamerafunktion med uppladdning till Firebase

--- app/src/main/java/com/example/splittab/ReceiptFragment.java
+++ app/src/main/java/com/example/splittab/ReceiptFragment.java
@@ -8,12 +8,18 @@ import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -25,46 +31,80 @@ import java.util.UUID;
 
 
 public class ReceiptFragment extends Fragment {
+
     private ImageView receiptImageview;
     private Button takePhotoButton;
     private Button uploadPhotoButton;
-    private static int REQUEST_PICTURE_CAPTURE = 1;
+    private static final int REQUEST_PICTURE_CAPTURE = 1;
     private FirebaseStorage storage = FirebaseStorage.getInstance();
-
+    private StorageReference mStorageRef;
+    private Bitmap picture;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.receipt_layout, container, false);
+
         receiptImageview = view.findViewById(R.id.receipt_imageview);
         takePhotoButton = view.findViewById(R.id.take_photo_button);
         uploadPhotoButton = view.findViewById(R.id.upload_photo_button);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
-        takePhotoButton.setOnClickListener(takePhotoLButtonListener);
-        //uploadPhotoButton.setOnClickListener(uploadPhotoLButtonListener);
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCamera();
+            }
+        });
+        uploadPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadPhoto();
+            }
+        });
 
         return view;
     }
-    private View.OnClickListener takePhotoLButtonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //startCamera();
+    private void startCamera(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent,REQUEST_PICTURE_CAPTURE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_PICTURE_CAPTURE && resultCode == -1){
+            Bundle extras = data.getExtras();
+            picture = (Bitmap)extras.get("data");
+            receiptImageview.setImageBitmap(picture);
         }
-    };
+    }
 
-    /*private View.OnClickListener uploadPhotoLButtonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Bitmap capture = Bitmap.createBitmap(receiptImageview.getWidth(),receiptImageview.getHeight(), Bitmap.Config.ARGB_8888);
+    private void uploadPhoto(){
+        final ProgressBar progressBar = getView().findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            capture.compress(Bitmap.CompressFormat.JPEG, 100,outputStream);
-            byte[] data = outputStream.toByteArray();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        picture.compress(Bitmap.CompressFormat.JPEG, 100,outputStream);
 
-            String path = "firebasephotos/"+ UUID.randomUUID() + ".jpeg";
-            StorageReference pathRef = storage.getReference(path);
+        String path = "firebasephotos/"+ UUID.randomUUID() + ".jpeg";
+        StorageReference pathRef = storage.getReference(path);
 
-            UploadTask uploadTask = pathRef.putBytes(data);
-        }
-    };*/
+        byte[] data = outputStream.toByteArray();
+        pathRef.putBytes(data)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressBar.setVisibility(View.GONE);
+
+                        taskSnapshot.getMetadata().getReference().getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Uri downloadUri = uri;
+                            }
+                        });
+                        Toast.makeText(getActivity().getBaseContext(),"Photo Uploaded",Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 
 }
